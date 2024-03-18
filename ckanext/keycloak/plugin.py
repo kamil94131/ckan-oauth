ccf240c8e47fb8a3c0eef1e8c51014c32182e319
Kamil Konopka <kamil.konopka@q-perior.com>
2024-03-18 14:05:54 +0100

    test2

--- ckanext/keycloak/plugin.py
+++ ckanext/keycloak/plugin.py
@@ -1,14 +1,20 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.types import Context
+from typing import Any
 
 from ckanext.keycloak.views import get_blueprint
 from ckanext.keycloak import helpers as h
+import ckan.plugins as p
+ignore_empty = p.toolkit.get_validator('ignore_empty')
+unicode_safe = p.toolkit.get_validator('unicode_safe')
 
 
 class KeycloakPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IResourceView, inherit=True)
 
     # IConfigurer
 
@@ -27,3 +33,19 @@ class KeycloakPlugin(plugins.SingletonPlugin):
             'button_style': h.button_style,
             'enable_internal_login': h.enable_internal_login,
         }
+    
+    def info(self) -> 'dict[str, Any]':
+        return {'name': 'test1',
+                'title': p.toolkit._('Website'),
+                'schema': {'page_url': [ignore_empty, unicode_safe]},
+                'iframed': False,
+                'icon': 'link',
+                'always_available': True,
+                'default_title': p.toolkit._('Website'),
+                }
+
+    def view_template(self, context: Context, data_dict: 'dict[str, Any]'):
+        return 'user/test1.html'
+
+    def form_template(self, context: Context, data_dict: 'dict[str, Any]'):
+        return 'user/test1.html'
