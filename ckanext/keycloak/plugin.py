0dc432601da72434c708b56c81dc56f39cd27db7
Kamil Konopka <kamil.konopka@q-perior.com>
2024-03-21 10:16:32 +0100

    test1

--- ckanext/keycloak/plugin.py
+++ ckanext/keycloak/plugin.py
@@ -4,6 +4,11 @@ import ckan.plugins.toolkit as toolkit
 from ckanext.keycloak.views import get_blueprint
 from ckanext.keycloak import helpers as h
 import ckan.plugins as p
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+
 ignore_empty = p.toolkit.get_validator('ignore_empty')
 unicode_safe = p.toolkit.get_validator('unicode_safe')
 
@@ -16,6 +21,12 @@ class KeycloakPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
+
+        app.logger.setLevel(logging.DEBUG)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.DEBUG)
+        app.logger.addHandler(stream_handler)
+
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'keycloak')
